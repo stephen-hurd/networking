25df564a021fc47f20af10f73145f4cc7c934c51
Matthew Macy <mmacy@kvasirtech.com>
2017-01-05 15:32:52 -0800

    rename iflib_ to regular file names, remove igb and lem files

--- sys/dev/e1000/if_igb.c
+++
@@ -1,9 +0,0 @@
-#ifndef KLD_MODULE
-#include "opt_iflib.h"
-#endif
-
-#ifdef IFLIB
-#include <dev/e1000/iflib_if_igb.c>
-#else
-#include <dev/e1000/legacy_if_igb.c>
-#endif
--- sys/dev/e1000/if_igb.h
+++
@@ -1,9 +0,0 @@
-#ifndef KLD_MODULE
-#include "opt_iflib.h"
-#endif
-
-#ifdef IFLIB
-#include <dev/e1000/iflib_if_igb.h>
-#else
-#include <dev/e1000/legacy_if_igb.h>
-#endif
