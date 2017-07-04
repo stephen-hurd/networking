1603fa99b76e9f582a33d650f712266330131e3c
Matt Macy <mmacy@matmacy.io>
2017-07-04 15:59:01 -0700

    don't split tx/rx interrupts

--- sys/dev/e1000/if_em.c
+++ sys/dev/e1000/if_em.c
@@ -1902,6 +1902,88 @@ em_allocate_pci_resources(if_ctx_t ctx)
 	return (0);
 }
 
+static int
+igb_intr_assign(if_ctx_t ctx, int msix)
+{
+        struct adapter *adapter = iflib_get_softc(ctx);
+        struct em_rx_queue *rx_que = adapter->rx_queues;
+        struct em_tx_queue *tx_que = adapter->tx_queues;
+        int error, rid, i, vector = 0, rx_vectors;
+        char buf[16];
+
+        /* First set up ring resources */
+        for (i = 0; i < adapter->rx_num_queues; i++, rx_que++, vector++) {
+                rid = vector + 1;
+                snprintf(buf, sizeof(buf), "rxq%d", i);
+                error = iflib_irq_alloc_generic(ctx, &rx_que->que_irq, rid, IFLIB_INTR_RXTX, em_msix_que, rx_que, rx_que->\
+me, buf);
+                if (error) {
+                        device_printf(iflib_get_dev(ctx), "Failed to allocate que int %d err: %d", i, error);
+                        adapter->rx_num_queues = i + 1;
+                        goto fail;
+                }
+
+                rx_que->msix =  vector;
+
+                /*
+                 * Set the bit to enable interrupt 
+                 * in E1000_IMS -- bits 20 and 21
+		 * are for RX0 and RX1, note this has
+		 * NOTHING to do with the MSIX vector
+                 */
+                if (adapter->hw.mac.type == e1000_82574) {
+                        rx_que->eims = 1 << (20 + i);
+                        adapter->ims |= rx_que->eims;
+                        adapter->ivars |= (8 | rx_que->msix) << (i * 4);
+                } else if (adapter->hw.mac.type == e1000_82575)
+                        rx_que->eims = E1000_EICR_TX_QUEUE0 << vector;
+                else
+                        rx_que->eims = 1 << vector;
+        }
+        rx_vectors = vector;
+
+        vector = 0;
+        for (i = 0; i < adapter->tx_num_queues; i++, tx_que++, vector++) {
+                rid = vector + 1;
+                snprintf(buf, sizeof(buf), "txq%d", i);
+                tx_que = &adapter->tx_queues[i];
+                iflib_softirq_alloc_generic(ctx, rid, IFLIB_INTR_TX, tx_que, tx_que->me, buf);
+
+                tx_que->msix = (vector % adapter->tx_num_queues);
+
+                if (adapter->hw.mac.type == e1000_82574) {
+                        tx_que->eims = 1 << (22 + i);
+                        adapter->ims |= tx_que->eims;
+                        adapter->ivars |= (8 | tx_que->msix) << (8 + (i * 4));
+                } else if (adapter->hw.mac.type == e1000_82575) {
+                        tx_que->eims = E1000_EICR_TX_QUEUE0 << (i %  adapter->tx_num_queues);
+                } else {
+                        tx_que->eims = 1 << (i %  adapter->tx_num_queues);
+                }
+        }
+
+        /* Link interrupt */
+        rid = rx_vectors + 1;
+        error = iflib_irq_alloc_generic(ctx, &adapter->irq, rid, IFLIB_INTR_ADMIN, em_msix_link, adapter, 0, "aq");
+
+        if (error) {
+                device_printf(iflib_get_dev(ctx), "Failed to register admin handler");
+                goto fail;
+        }
+        adapter->linkvec = rx_vectors;
+        if (adapter->hw.mac.type < igb_mac_min) {
+                adapter->ivars |=  (8 | rx_vectors) << 16;
+                adapter->ivars |= 0x80000000;
+        }
+        return (0);
+fail:
+        iflib_irq_free(ctx, &adapter->irq);
+        rx_que = adapter->rx_queues;
+        for (int i = 0; i < adapter->rx_num_queues; i++, rx_que++)
+                iflib_irq_free(ctx, &rx_que->que_irq);
+        return (error);
+}
+
 /*********************************************************************
  *
  *  Setup the MSIX Interrupt handlers
@@ -1915,6 +1997,10 @@ em_if_msix_intr_assign(if_ctx_t ctx, int msix)
 	struct em_tx_queue *tx_que = adapter->tx_queues;
 	int error, rid, i, vector = 0;
 	char buf[16];
+
+	if (adapter->hw.mac.type >= igb_mac_min) {
+		return igb_intr_assign(ctx, msix);
+	}
 
 	/* First set up ring resources */
 	for (i = 0; i < adapter->rx_num_queues; i++, rx_que++, vector++) {
