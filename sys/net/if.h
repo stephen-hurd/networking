f51d102dc2ff56824aaa8892608970176fcd97bb
Matthew Macy <mmacy@mattmacy.io>
2017-08-27 12:14:58 -0700

    make flag defines line up and add NO_ENTROPY flag

--- sys/net/if.h
+++ sys/net/if.h
@@ -137,29 +137,29 @@ struct if_data {
  * (d) if_drv_flags field written only by the device driver, read by either
  *     the stack or driver.
  */
-#define	IFF_UP		0x1		/* (n) interface is up */
-#define	IFF_BROADCAST	0x2		/* (i) broadcast address valid */
-#define	IFF_DEBUG	0x4		/* (n) turn on debugging */
-#define	IFF_LOOPBACK	0x8		/* (i) is a loopback net */
-#define	IFF_POINTOPOINT	0x10		/* (i) is a point-to-point link */
-/*			0x20		   was IFF_SMART */
-#define	IFF_DRV_RUNNING	0x40		/* (d) resources allocated */
-#define	IFF_NOARP	0x80		/* (n) no address resolution protocol */
-#define	IFF_PROMISC	0x100		/* (n) receive all packets */
-#define	IFF_ALLMULTI	0x200		/* (n) receive all multicast packets */
-#define	IFF_DRV_OACTIVE	0x400		/* (d) tx hardware queue is full */
-#define	IFF_SIMPLEX	0x800		/* (i) can't hear own transmissions */
-#define	IFF_LINK0	0x1000		/* per link layer defined bit */
-#define	IFF_LINK1	0x2000		/* per link layer defined bit */
-#define	IFF_LINK2	0x4000		/* per link layer defined bit */
+#define	IFF_UP		0x00000001		/* (n) interface is up */
+#define	IFF_BROADCAST	0x00000002		/* (i) broadcast address valid */
+#define	IFF_DEBUG	0x00000004		/* (n) turn on debugging */
+#define	IFF_LOOPBACK	0x00000008		/* (i) is a loopback net */
+#define	IFF_POINTOPOINT	0x00000010		/* (i) is a point-to-point link */
+#define	IFF_NO_ENTROPY	0x00000020		/* (d) disable entropy collection */
+#define	IFF_DRV_RUNNING	0x00000040		/* (d) resources allocated */
+#define	IFF_NOARP	0x00000080		/* (n) no address resolution protocol */
+#define	IFF_PROMISC	0x00000100		/* (n) receive all packets */
+#define	IFF_ALLMULTI	0x00000200		/* (n) receive all multicast packets */
+#define	IFF_DRV_OACTIVE	0x00000400		/* (d) tx hardware queue is full */
+#define	IFF_SIMPLEX	0x00000800		/* (i) can't hear own transmissions */
+#define	IFF_LINK0	0x00001000		/* per link layer defined bit */
+#define	IFF_LINK1	0x00002000		/* per link layer defined bit */
+#define	IFF_LINK2	0x00004000		/* per link layer defined bit */
 #define	IFF_ALTPHYS	IFF_LINK2	/* use alternate physical connection */
-#define	IFF_MULTICAST	0x8000		/* (i) supports multicast */
-#define	IFF_CANTCONFIG	0x10000		/* (i) unconfigurable using ioctl(2) */
-#define	IFF_PPROMISC	0x20000		/* (n) user-requested promisc mode */
-#define	IFF_MONITOR	0x40000		/* (n) user-requested monitor mode */
-#define	IFF_STATICARP	0x80000		/* (n) static ARP */
-#define	IFF_DYING	0x200000	/* (n) interface is winding down */
-#define	IFF_RENAMING	0x400000	/* (n) interface is being renamed */
+#define	IFF_MULTICAST	0x00008000		/* (i) supports multicast */
+#define	IFF_CANTCONFIG	0x00010000		/* (i) unconfigurable using ioctl(2) */
+#define	IFF_PPROMISC	0x00020000		/* (n) user-requested promisc mode */
+#define	IFF_MONITOR	0x00040000		/* (n) user-requested monitor mode */
+#define	IFF_STATICARP	0x00080000		/* (n) static ARP */
+#define	IFF_DYING	0x00200000	/* (n) interface is winding down */
+#define	IFF_RENAMING	0x00400000	/* (n) interface is being renamed */
 /*
  * Old names for driver flags so that user space tools can continue to use
  * the old (portable) names.
